d35cc0e4acb48809d25c1163d20838285c1ff7c7
Hugo Tinoco <htinoco@devpop.localdomain>
2022-02-20 21:48:49 -0700

    checkpoint

--- terraform-plans/do-droplet/main.tf
+++ terraform-plans/do-droplet/main.tf
@@ -7,31 +7,26 @@ resource "digitalocean_droplet" "nautobot-apps" {
   ssh_keys = [
     data.digitalocean_ssh_key.devpop.id
   ]
-  user_data = file("data/nautobot-droplets.yml")
   
-  // provisioner "remote-exec" {
-  //   inline = ["sudo apt update", "sudo apt install python3 -y", "echo Done!"]
+  provisioner "remote-exec" {
+    inline = ["sudo apt update", "sudo apt install python3 tree -y", "echo Done!"]
 
-  //   connection {
-  //     host        = self.ipv4_address
-  //     type        = "ssh"
-  //     user        = "root"
-  //     private_key = file(var.PVT_KEY)
-  //   }
-  // }
+    connection {
+      host        = self.ipv4_address
+      type        = "ssh"
+      user        = "root"
+      private_key = file(var.PVT_KEY)
+    }
+  }
 
-  // provisioner "local-exec" {
-  //   command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${self.ipv4_address},' --private-key ${var.PVT_KEY} -e 'pub_key=${var.PUB_KEY}' baseline-configuration.yml"
-  // }
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${self.ipv4_address},' --private-key ${var.PVT_KEY} -e 'pub_key=${var.PUB_KEY}' baseline-configuration.yml"
+  }
 }
 
 output "droplet_ip_addresses" {
   value = {
-<<<<<<< HEAD
     for droplet in digitalocean_droplet.nautobot-apps :
-=======
-    for droplet in digitalocean_droplet.web :
->>>>>>> 312aa16a1344e800b2284be5e94b956834ae3ecc
     droplet.name => droplet.ipv4_address
   }
 }
